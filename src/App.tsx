d51fc988632e2c18b765501609be7f8305a1bf6e
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-16 19:22:34 -0700

    [ADD] Maquetado el modal, agredado handles para la captura de datos y ya se escriben en el BudgetProvider

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,11 @@ function App() {
     <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
       {isValidBudget ? <BudgetTracker/> : <BudgetForm/>}
     </div>
-
-    <ExpenseModal/>
+    {isValidBudget && (
+      <main className="max-w-3xl mx-auto py-10">
+        <ExpenseModal/>
+      </main>
+    )}
     </>
   )
 }
--- src/reducers/budger-reducers.ts
+++ src/reducers/budger-reducers.ts
@@ -1,12 +1,30 @@
+import {v4 as uuidv4} from 'uuid'
+import { Expense } from "../types"
+import { DraftExpense } from '../types/index';
+
 export type BudgetActions = 
-    {type: 'define-budget', payload: {budget: number}}
+    {type: 'define-budget', payload: {budget: number}} |
+    {type: 'show-modal'} |
+    {type: 'close-modal'} |
+    {type: 'add-expense', payload: {expense: DraftExpense}} 
 
 export type BudgetState = {
-    budget: number
+    budget: number,
+    modal: boolean,
+    expenses: Expense[]
 }
 
 export const initialState : BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false,
+    expenses: []
+}
+
+const createExpense = (draftExpense: DraftExpense) : Expense => {
+    return {
+        ...draftExpense,
+        id: uuidv4()
+    }
 }
 
 export const BudgetReducer = (
@@ -18,6 +36,28 @@ export const BudgetReducer = (
         return {
             ...state,
             budget: actions.payload.budget
+        }
+    }
+
+    if(actions.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
+        }
+    }
+
+    if(actions.type === 'close-modal') {
+        return {
+            ...state,
+            modal: false
+        }
+    }
+
+    if(actions.type === 'add-expense') {
+        const expense = createExpense(actions.payload.expense)
+        return {
+            ...state,
+            expenses: [...state.expenses, expense]
         }
     }
 
