202898bfa4b6b8ef52fe5248d6d1c0a7fafc2347
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-18 21:37:16 -0700

    [ADD] Se muestra el listado de gastos cuando se agrega un nuevo gasto

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -4,7 +4,7 @@ export default function BudgetTracker() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
-                <img src="/public/grafico.jpg" alt="" />
+                <img src="/grafico.jpg" alt="" />
             </div>
 
             <div className="flex flex-col justify-center items-center gap-8">
